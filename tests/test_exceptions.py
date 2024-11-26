94f8b2ad405285f53ff5874bd86d77dc93a48a9f
Anton Dubovik <anton_dubovik@epam.com>
2024-11-26 11:51:02 +0000

    fix: unified test exceptions tests into one

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -12,7 +12,7 @@ from tests.utils.chunks import create_chunk_checker, create_sse_stream_checker
 from tests.utils.dial_app import create_httpx_client
 from tests.utils.json import has_type, match_objects, memorize
 
-to_many_requests_error = _parse_dial_exception(
+_too_many_requests_error = _parse_dial_exception(
     status_code=http.HTTPStatus.TOO_MANY_REQUESTS,
     content={"error": {"message": "Too many requests"}},
     headers={"retry-after": "42"},
@@ -20,7 +20,7 @@ to_many_requests_error = _parse_dial_exception(
 
 
 @pytest.mark.parametrize("stream", [False, True])
-@pytest.mark.parametrize("repeats", [1, 2, 3])
+@pytest.mark.parametrize("repeats", [0, 1, 2, 3])
 async def test_interceptor_errors(stream: bool, repeats: int):
     httpx_client = create_httpx_client(
         [
@@ -28,7 +28,7 @@ async def test_interceptor_errors(stream: bool, repeats: int):
                 "upstream",
                 (
                     "chat/completions",
-                    create_broken_application(to_many_requests_error),
+                    create_broken_application(_too_many_requests_error),
                 ),
             ),
             ("no-op", ChatCompletionNoOpInterceptor),
@@ -76,80 +76,25 @@ async def test_interceptor_errors(stream: bool, repeats: int):
             created=created_checker,
         )
 
-        expected = create_sse_stream_checker(
-            chunk_checker(),
-            {
-                # FIXME: error chunks shouldn't have id/created/object fields
-                # https://github.com/epam/ai-dial-sdk/blob/development/aidial_sdk/chat_completion/chunks.py#L31-L35
-                # Alternatively, the errors in the stream should be
-                # translated to DIALExceptions in the Interceptors SDK
+        if repeats == 0:
+            extra_fields = {}
+        else:
+            # FIXME: The errors in the stream should be
+            # translated to DIALExceptions in the Interceptors SDK instead of
+            # being used as if they are normal chat completion chunks in
+            # the method `handle_streaming_errors`.
+            extra_fields = {
                 "id": id_checker,
                 "created": created_checker,
                 "object": "chat.completion.chunk",
-                "error": {"message": "Too many requests", "code": "429"},
-            },
-            "[DONE]",
-        )
-
-        match_objects(actual, expected)
-
-
-@pytest.mark.parametrize("stream", [False, True])
-async def test_direct_errors(stream: bool):
-    httpx_client = create_httpx_client(
-        [
-            (
-                "upstream",
-                (
-                    "chat/completions",
-                    create_broken_application(to_many_requests_error),
-                ),
-            ),
-            ("no-op", ChatCompletionNoOpInterceptor),
-        ],
-        ["upstream"],
-    )
-
-    response = await httpx_client.post(
-        "chat/completions",
-        json={
-            "stream": stream,
-            "messages": [{"role": "user", "content": "hello"}],
-        },
-    )
-
-    if not stream:
-        assert response.status_code == 429
-        actual_headers = {
-            k.decode(): v.decode() for k, v in response.headers.raw
-        }
-        assert match_objects(
-            actual_headers,
-            {
-                "content-length": has_type(str),
-                "content-type": "application/json",
-                "retry-after": "42",
-            },
-        )
-        assert response.json() == {
-            "error": {"message": "Too many requests", "code": "429"}
-        }
-    else:
-        assert response.status_code == 200
-
-        actual = [line async for line in response.aiter_lines()]
-
-        id_checker = memorize(has_type(str))
-        created_checker = memorize(has_type(int))
-        chunk_checker = create_chunk_checker(
-            stream=stream,
-            id=id_checker,
-            created=created_checker,
-        )
+            }
 
         expected = create_sse_stream_checker(
             chunk_checker(),
-            {"error": {"message": "Too many requests", "code": "429"}},
+            {
+                **extra_fields,
+                "error": {"message": "Too many requests", "code": "429"},
+            },
             "[DONE]",
         )
 
