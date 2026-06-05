6e1801e9e34008416d6cd6b06caa4d28fd206310
Anton Dubovik <anton_dubovik@epam.com>
2026-06-05 12:34:51 +0100

    feat: fix text dependencies

--- tests/unit_tests/test_response_handler.py
+++ tests/unit_tests/test_response_handler.py
@@ -1,7 +1,7 @@
 import fastapi
 from aidial_sdk.chat_completion import Request, Response
+from aidial_sdk.pydantic_v1 import SecretStr
 from aidial_sdk.utils.streaming import to_block_response
-from pydantic import SecretStr
 
 from aidial_interceptors_sdk.chat_completion.annotated_value import (
     AnnotatedChunk,
@@ -52,7 +52,7 @@ async def get_response(cls: type[ResponseHandler], **kwargs) -> dict:
     dummy_request = Request(
         messages=[],
         stream=False,
-        api_key_secret=SecretStr("dummy"),
+        api_key_secret=SecretStr("dummy"),  # type: ignore
         deployment_id="dummy",
         headers={},
         original_request=fastapi.Request(scope={"type": "http"}),
