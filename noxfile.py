74d70cb31b2b3b7ad0a2bbb2e352c14e46766033
Anton Dubovik <anton_dubovik@epam.com>
2026-07-14 12:48:02 +0100

    feat: support python 3.13

--- noxfile.py
+++ noxfile.py
@@ -43,7 +43,7 @@ _PYDANTIC_DEPS: dict[str, tuple[str, str]] = {
 }
 
 
-@nox.session(python=["3.11", "3.12"])
+@nox.session(python=["3.11", "3.12", "3.13"])
 # Testing against earliest and latest supported versions of the dependencies
 @nox.parametrize("pydantic", ["1.10.17", "2.8.2"])
 @nox.parametrize("httpx", ["0.25.0", "0.27.0"])
